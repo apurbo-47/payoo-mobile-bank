9f03b9d2570e2f87481e7556ab27758a0399b683
Your Name <jahedulislamapurbo@gmail.com>
2025-10-29 17:08:40 +0600

    cashout section

---
+++ script/login.js
@@ -0,0 +1,22 @@
+document.getElementById ('login-btn')
+.addEventListener ('click', 
+    function (event){
+        event.preventDefault();
+        const acountNumber = document.getElementById ('acount-number').value;
+        const pin = document.getElementById ('pin').value;
+        const convertedPin = parseInt(pin);
+       
+        if (acountNumber.length === 11) {
+             
+            if (convertedPin === 1234) {
+                window.location.href = "./main.html"
+            }
+            else {
+                alert ('pin incorrect');
+            }    
+        }
+
+        else {
+            alert ('need valid acount number');
+        }
+})
